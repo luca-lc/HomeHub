431c6d96185228665cfd5c8bb5cb2c0b20ea27ae
luca <lccanessa@gmail.com>
2021-01-17 22:37:58 +0100

    add automatic and interactive user creation

--- FTPHub/conf/config.sh
+++ FTPHub/conf/config.sh
@@ -11,7 +11,7 @@ loop=1
 while[[ $loop -le $nusers ]]
 do
 	if [[ -z "$uname$loop" || -z "$upass$loop" ]]; then
-		echo -e "Impossible to proceed: invalid name or password"
+		echo -e "Impossible to proceed: invalid name or password for $uname$loop"
 	else
 		useradd -ms /bin/bash -p "$(openssl passwd -1 $upass$loop)" -U $uname$loop # creation of the user and his home
 	fi
