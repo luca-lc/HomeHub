1697a0e604115ada064733ec6472dcc1348c8bf6
luca <lccanessa@gmail.com>
2021-01-17 01:15:36 +0100

    fixed minor bugs - added a variables control

--- FTPHub/conf/config.sh
+++ FTPHub/conf/config.sh
@@ -2,9 +2,16 @@
 
 # read config file
 . /home/configFile.conf
+# . ./configFile.conf
+
+if [[ -z "$gpassword" || -z "$gname" ]]; then
+	echo -e "Impossible to proceed: invalid name or password"
+else
+	useradd -ms /bin/bash -p "$(openssl passwd -1 $gpassword)" -U $gname
+fi
 
 # guest user creations
-useradd -ms /bin/bash -p "$(openssl passwd -1 $gpassword)" -U $gname
+#useradd -ms /bin/bash -p "$(openssl passwd -1 $gpassword)" -U $gname
 
 # user1 creations
 # useradd -ms /bin/bash -p "$(openssl passwd -1 $upassword)" -U $uname
