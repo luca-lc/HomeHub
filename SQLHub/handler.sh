278e65c8e9059d9aad27f08abdd65e9846160527
luca <lccanessa@gmail.com>
2021-02-22 00:35:19 +0100

    security updating

--- SQLHub/handler.sh
+++ SQLHub/handler.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-host=localhost
-database=''
-user=''
-passwd=''
-rootpswd=''
+host=127.0.0.1
 
 
 function usage( )
@@ -13,46 +9,85 @@ function usage( )
 	}
 
 
-function getVar( )
-	{
-		echo "w"
-	}
-
-
+# CREATES A NEW USER
 function newUser( )
 	{
-		getVar
-		command="CREATE USER '${user}'@'${host}' IDENTIFIED BY '${passwd}'; REVOKE ALL ON * FROM '${user}'@'${host}'; GRANT ALL privileges ON \`${database}\`.* TO '${user}'@'${host}';FLUSH PRIVILEGES;"
+		database=$1
+		user=$2
+		passwd=$3
 
-		echo "${commands}" | /usr/bin/mysql -u root -p
+		command="CREATE USER ${user}@${host} IDENTIFIED BY ${passwd}; REVOKE ALL ON *.* FROM ${user}@${host}; GRANT ALL ON ${database}.* TO ${user}@${host}; FLUSH PRIVILEGES;"
+
+		echo "${command}" | /usr/bin/mysql -u root -p
+
 	}
 
+# CREATES A NEW DATABASE
 function newDB( )
 	{
-		echo "e"
+		database=$1
+		user=$2
+		passwd=$3
+
+		command="CREATE DATABASE ${database};GRANT ALL ON ${database}.* TO ${user}@${host} IDENTIFIED BY ${passwd};FLUSH PRIVILEGES;"
+
+		echo "${command}" | /usr/bin/mysql -u root -p
 	}
 
+# CREATE A NEW USER AND A NEW DATABASE
 function newServ( )
 	{
-		echo "r"
+		database=$1
+		user=$2
+		passwd=$3
+
+		command="CREATE DATABASE ${database};CREATE USER ${user}@${host} IDENTIFIED BY ${passwd};REVOKE ALL ON *.* FROM ${user}@${host};GRANT ALL ON ${database}.* TO ${user}@${host} IDENTIFIED BY ${passwd};FLUSH PRIVILEGES;"
+
+		echo "${command}" | /usr/bin/mysql -u root -p
+	}
+
+# ASSIGNS AN USER TO A DATABASE
+function userDB( )
+	{
+		database=$1
+		user=$2
+		passwd=$3
+
+		command="GRANT ALL ON ${database}.* TO ${user}@${host} IDENTIFIED BY ${passwd};FLUSH PRIVILEGES;"
+
+		echo "${command}" | /usr/bin/mysql -u root -p
 	}
 
 
 if [ $# -eq 1 ]; then
 		if [ -n "$1" ]; then
-			case "$1" in
-				-nu) newUser
-				;;
-				-nd) newDB
-				;;
-				-nn) newServ
-				;;
-				-h) usage
-				;;
-				*) 	echo -e "Option $1 not recognized"
-						echo "Try '$0 -h' for more information."
-				;;
-			esac	
+			read -p "Database: " database
+			read -p "User: " user
+			read -sp "Password: " passwd 
+			echo
+			read -sp "Repeat Password: " rpasswd  
+			echo
+			
+			if [[ $passwd == $rpasswd ]]; then
+				case "$1" in
+					-nu) newUser $database $user $passwd
+					;;
+					-nd) newDB $database $user $passwd
+					;;
+					-nn) newServ $database $user $passwd
+					;;
+					-ud) userDB $database $user $passwd
+					;;
+					-h) usage
+					;;
+					*) 	echo -e "Option $1 not recognized"
+							echo "Try '$0 -h' for more information."
+					;;
+				esac
+			else
+				echo -e "Passwords do not match\n"
+				exit 1
+			fi
 		fi
 	else
 		echo "missing arguments" 
